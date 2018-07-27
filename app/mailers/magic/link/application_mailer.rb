a9478d7cbc0a734201d034194460f7c2552a586e
QuintinAdam <quintinjadam@gmail.com>
2018-07-27 15:14:38 +0100

    MagicLinkMailer inherited from DeviseMailer. I18n for mailer text.

--- app/mailers/magic/link/application_mailer.rb
+++
@@ -1,8 +0,0 @@
-module Magic
-  module Link
-    class ApplicationMailer < ::ApplicationMailer
-      default from: Magic::Link.email_from
-      layout 'mailer'
-    end
-  end
-end
--- app/mailers/magic/link/magic_link_mailer.rb
+++ app/mailers/magic/link/magic_link_mailer.rb
@@ -1,9 +1,10 @@
 module Magic::Link
-  class MagicLinkMailer < ApplicationMailer
+  class MagicLinkMailer < ::Devise::Mailer
+    default from: Magic::Link.email_from
     def send_magic_link(email, token)
       @email = email
       @token = token
-      mail(to: email, subject: "Your sign in link")
+      mail(to: email, subject: I18n.t('magic-link.mailer.subject'))
     end
   end
 end
