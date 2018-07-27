85f263c8fc0727f1239285a0aa16af4ebe744592
QuintinAdam <quintinjadam@gmail.com>
2018-07-27 15:10:51 +0100

    MagicLinkController inherited from DeviseController

--- app/controllers/magic/link/magic_links_controller.rb
+++ app/controllers/magic/link/magic_links_controller.rb
@@ -1,6 +1,6 @@
 module Magic
   module Link
-    class MagicLinksController < ::ApplicationController
+    class MagicLinksController < ::DeviseController
       before_action :check_user, only: :new
 
       def new
